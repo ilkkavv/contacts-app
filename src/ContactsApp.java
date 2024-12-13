14200ff33d94c6fea4cc5bb0901616eeb5ce0574
Ilkka Wahala <ilkka.wahala@tuni.fi>
2024-12-13 15:28:09 +0200

    feat: add creation of contacts database and gui

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,5 +1,12 @@
-public class ContactsApp {
-    public static void main(final String[] args) {
+package src;
 
+public class ContactsApp {
+    // Main loop
+    public static void main(final String[] args) {
+        ContactDatabase database = new ContactDatabase();
+
+        GuiFrame contactsApp = new GuiFrame("Contacts App", 480, 854, database);
+
+        contactsApp.openGui();
     }
 }
